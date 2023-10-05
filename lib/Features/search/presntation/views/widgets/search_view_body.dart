688be746a9be5494a22433151299925ad3db8859
MAHMOUDGERMY <germymahmod@gmail.com>
2023-10-05 22:44:44 +0300

    start with domain layer

--- lib/Features/search/presntation/views/widgets/search_view_body.dart
+++ lib/Features/search/presntation/views/widgets/search_view_body.dart
@@ -1,32 +1,48 @@
+import 'package:books_app/Features/home/presntation/views/widgets/best_seller_listview_item.dart';
+import 'package:books_app/Features/search/presntation/views/widgets/custom_search_textfield.dart';
+import 'package:books_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [CustomSearchTextField()],
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 30),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomSearchTextField(),
+          SizedBox(
+            height: 16,
+          ),
+          Text(
+            "Search Results",
+            style: Styles.textStyle18,
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Expanded(child: SearchListView()),
+        ],
+      ),
     );
   }
 }
 
-class CustomSearchTextField extends StatelessWidget {
-  const CustomSearchTextField({super.key});
+class SearchListView extends StatelessWidget {
+  const SearchListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      decoration: InputDecoration(
-          hintText: "Search",
-          suffixIcon: IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              FontAwesomeIcons.magnifyingGlass,
-              size: 22,
-            ),
-          )),
+    return ListView.builder(
+      padding: EdgeInsets.zero,
+      itemCount: 10,
+      itemBuilder: (context, index) => const Padding(
+        padding: EdgeInsets.symmetric(vertical: 10),
+        child: BookListViewItem(),
+      ),
     );
   }
 }
