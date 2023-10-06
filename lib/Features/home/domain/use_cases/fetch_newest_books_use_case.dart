54366e43555a88e0fdcf82584239f94eb4e51e64
MAHMOUDGERMY <germymahmod@gmail.com>
2023-10-06 14:26:43 +0300

    fetch newest books use case

---
+++ lib/Features/home/domain/use_cases/fetch_newest_books_use_case.dart
@@ -0,0 +1,17 @@
+import 'package:books_app/Features/home/domain/entities/book_entity.dart';
+import 'package:books_app/Features/home/domain/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/errors/failure.dart';
+import '../../../../core/use_cases/use_case.dart';
+
+class FetchNewstBooksUseCase extends UseCase<List<BookEntity>, NoParam> {
+  final HomeRepo homeRepo;
+
+  FetchNewstBooksUseCase({required this.homeRepo});
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([NoParam? param]) async {
+    return await homeRepo.fetchNewestBooks();
+  }
+}
